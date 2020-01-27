293c487b5f1095c9bfeba417b9a94811b17cc9c7
AurioPinto <auriopinto@outlook.com>
2020-01-28 01:58:37 +0800

    upload the init.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
-import pandas
-import numpy
-from tqdm import tqdm
-for i in tqdm(range(100)):
-    ...
+
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+tf.enable_resource_variables()
+tf.enable_eager_execution()
+try:
+    import t3f
+except ImportError:
+    # Install T3F if it's not already installed.
+    # !git clone https: // github.com/Bihaqo/t3f.git
+    # !cd t3f
+    # pip install .
+    import t3f
