4d41a1f53f6d59853667dda7d649eac1c5734d73
AurioPinto <auriopinto@outlook.com>
2020-01-30 09:38:43 +0800

    Upload

--- __init__.py
+++ __init__.py
@@ -1,15 +1,29 @@
-
-
+import matplotlib
 import numpy as np
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-tf.enable_resource_variables()
-tf.enable_eager_execution()
-try:
-    import t3f
-except ImportError:
-    # Install T3F if it's not already installed.
-    # !git clone https: // github.com/Bihaqo/t3f.git
-    # !cd t3f
-    # pip install .
-    import t3f
+import matplotlib.pyplot as plt
+
+np.random.seed(19680801)
+
+# example data
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+x = mu + sigma * np.random.randn(437)
+
+num_bins = 50
+
+fig, ax = plt.subplots()
+
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, density=1)
+
+# add a 'best fit' line
+y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+     np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+ax.plot(bins, y, '--')
+ax.set_xlabel('Smarts')
+ax.set_ylabel('Probability density')
+ax.set_title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
